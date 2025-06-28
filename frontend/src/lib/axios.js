9298cba55e7c9eebc7155cdcfc91c597f65e6e43
Anita <siddharthjaiswar30495@gmail.com>
2025-06-28 15:38:24 +0530

    prepared for the deployment

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+const Base_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
 const api = axios.create({
     baseURL : "http://localhost:5001/api",
 });
