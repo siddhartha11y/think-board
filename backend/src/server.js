b9a942c01d0705ac455ef481d14d71664a864529
Anita <siddharthjaiswar30495@gmail.com>
2025-06-28 16:31:54 +0530

    server updated

--- backend/src/server.js
+++ backend/src/server.js
@@ -35,9 +35,9 @@ app.use("/api/notes", notesRoutes);
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist/index.html"));
+});
 }
 
 connectDB().then(() => {
